10c4414fcb447c63bd44048b6b10cc8be62e8130
Sean Cherng <seancherng.tw@gmail.com>
2021-04-09 17:31:54 +0800

    Rearrange folders

--- 0347/main.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counter = {}
-        for num in nums:
-            counter[num] = counter.get(num, 0) + 1
-        counter = sorted(counter.items(), key=lambda item: item[1], reverse=True)
-        return [num for num, count in counter[:k]]
